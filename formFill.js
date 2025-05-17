8b8c228697f669bf3b674cdc1b25411b25860e1d
calebgeb12 <calebgeb55@gmail.com>
2025-05-17 13:59:34 -0400

    finished logic for majority of "my information" section, currently stuck on inputting resume

--- formFill.js
+++ formFill.js
@@ -2,46 +2,104 @@ export default function () {
     
     //hashmap below contains answer to all questions (maybe?)
     let hashMap = {
-        //container questions
+    //manual typing container questions (single)
         "How Did You Hear About Us?*": [chooseFromContainer, "Other"],
+        // "Country Phone Code*": [chooseFromContainer, "United States of America (+1)"],
+        "Country Phone Code*": [doNothing, "United States of America (+1)"],
 
-        //choose from list questions
+    //manual typing container questions (multiple) 
+        "Type to Add Skills": [chooseMultipleFromContainer, ["Java", "Python", "C#", "JavaScript", "HTML/CSS", "Unity"]],
+
+    //choose from list questions (no typing)
         "State": [chooseFromList, "Georgia"],
         "State*": [chooseFromList, "Georgia"],
+        "Phone Device Type*": [chooseFromList, "Mobile"],
+        "Country*": [chooseFromList, "United States of America"],
+        "Country": [chooseFromList, "United States of America"],
 
-        //simple questions
+    //simple questions
         "First Name*": [enterAnswer, "Caleb"],
+        "First Name": [enterAnswer, "Caleb"],
+
         "Last Name*": [enterAnswer, "Gebremeskel"],
+        "Last Name": [enterAnswer, "Gebremeskel"],
+
         "Address Line 1": [enterAnswer, "1609 Virginia Pine Circle SW"],
         "Address Line 1*": [enterAnswer, "1609 Virginia Pine Circle SW"],
+
         "City": [enterAnswer, "Lilburn"],
         "City*": [enterAnswer, "Lilburn"],
+
         "Postal Code": [enterAnswer, "30047"],
         "Postal Code*": [enterAnswer, "30047"],
+
         "Zip Code": [enterAnswer, "30047"],
         "Zip Code*": [enterAnswer, "30047"],
 
-        //yes or no question
+        "Phone Number": [enterAnswer, "4047296027"],
+        "Phone Number*": [enterAnswer, "4047296027"],
 
-        //do nothing questions
-        "I have a preferred name": [doNothing, "useless"]
+        "LinkedIn": [enterAnswer, "www.linkedin.com/in/caleb-gebremeskel-7a626a324"],
+
+    //upload questions
+        "Resume/CV": [upload, "files/resume.pdf"],
+
+    //work experience questions
+        "Work Experience": [doNothing /**enterWorkExperience**/,[
+            {
+                "Job Title*": "IT Business Solutions Intern",
+                "Company*": "Gwinnett County Government",
+                "Location": "Lawrenceville, GA",
+                "I currently work here": false,
+                "From*": ["06", "2023"],
+                "To*": ["06", "2023"],
+                "Role Description": ["bullet point1", "bullet point 2", "bullet point 3"]
+            },
+
+            {
+                "Job Title*": "second job",
+                "Company*": "seocnd company",
+                "Location": "random place, GA",
+                "I currently work here": false,
+                "From*": ["01", "2022"],
+                "To*": ["06", "2024"],
+                "Role Description": ["b point1", "b point 2", "b point 3"]
+            }]
+        ],
+
+
+
+    //education questions
+
+
+
+    //yes or no question
+
+    //do nothing questions
+        "null": [doNothing, "useless"],
+        "I have a preferred name": [doNothing, "useless"],
+        "Phone Extension": [doNothing, "useless"],
+        "Facebook": [doNothing, "useless"],
+        "Twitter": [doNothing, "useless"]
     };
 
     //fills the current page
     function fillPage() {
-        //get list of all inputs, and associate them with their question
+        //get list of all inputs, and associate them with their question (some fields such as those for filling work experience or education sections are special cases)
         let inputsArray = Array.from(document.querySelectorAll('input'));
         let questionsArray = new Array(inputsArray.length); 
         
-        //detects dropdown box that have button with id instead of input with id
+        //detects dropdown box that have button with id instead of input with id (useful for questions where you choose from a list since they don't have visible input fields)
         for (let i = 0; i < inputsArray.length; i++) {
             let input = inputsArray[i];
             let btn = input.previousElementSibling?.tagName === 'BUTTON' ? input.previousElementSibling : input.nextElementSibling?.tagName === 'BUTTON' ? input.nextElementSibling : null;
             if (btn) { //is a dropdown
                 inputsArray[i] = btn;
             }
+
         }
         
+        //associate input field with its question
         for (let i = 0; i < inputsArray.length; i++) {
             let currInput = inputsArray[i];
             let inputId = currInput.id;
@@ -52,34 +110,86 @@ export default function () {
             }
             
             questionsArray[i] = labelText;
-            console.log(labelText);
         }
 
-        // for (let i = 0; i < inputsArray.length; i++) {
-        //     let currInput = inputsArray[i];
-        //     let currQuestion = questionsArray[i];
+        //find work experience, education, resume sections and add them to inputsArray (if they exist)
+        let uniqueQuestions = Array.from(document.querySelectorAll('h3#Work-Experience-section, h3#Education-section, h3#Resume\\/CV-section'));
+        for (let i = 0; i < uniqueQuestions.length; i++) {
+            let currQuestion = uniqueQuestions[i];
+            let neighborDiv = currQuestion.nextElementSibling?.tagName === 'DIV' ? currQuestion.nextElementSibling : null;
+            let currBtn = neighborDiv.querySelector('button');
+            inputsArray.push(currQuestion);
+            questionsArray.push(currQuestion.textContent.trim());
+        }
 
-        //     // //we have an answer
-        //     // if (hashMap.hasOwnProperty(currQuestion)) {
-        //     //     let currArray = hashMap[currQuestion];
-        //     //     let functionToRun = currArray[0];
-        //     //     let answer = currArray[1];
-        //     //     functionToRun(currInput, answer);
-        //     // }
+        //handle answering each question
+        let questions_answered = 0;
+        for (let i = 0; i < inputsArray.length; i++) {
+            let currInput = inputsArray[i];
+            let currQuestion = questionsArray[i];
 
-        //     // //unique question
-        //     // else {
+            //we have an answer
+            if (hashMap.hasOwnProperty(currQuestion)) {
+                let currArray = hashMap[currQuestion];
+                let functionToRun = currArray[0];
+                let answer = currArray[1];
+                console.log("existing question: " + currQuestion)
+                functionToRun(currInput, answer);
+                questions_answered++;
+            }
 
-        //     // }
-        // }
+            //unique question
+            else {
+                console.log("unique question: " + currQuestion);
+            }
+        }
+
+        //goes to the next page if all questions have been answered
+        let nextButton = document.querySelector('[data-automation-id="pageFooterNextButton"]');
+        setTimeout(() => {
+            //can go to next page
+            if (nextButton && questions_answered === questionsArray.length) {
+                nextButton.click();
+                console.log("All questions answered, going to next page...");
+                console.log("---------------------------");
+                
+                setTimeout(() => {
+                    fillPage();
+                }, 2000) //how quickly you start filling out form after clicking next button
+            }
+
+            //cannot go to next page 
+            else {
+                if (nextButton) {
+                    console.log("Not all questions have been answered");
+                }
+
+                else if (questions_answered === questionsArray.length) {
+                    console.log("next button not found");
+                }
+
+                else {
+                    console.log("next button not found AND not all questions have been answered");
+                }
+            }
+        }, 5000) //how quickly you go to the next page 
 
         //only for testing
-        let currInput = inputsArray[7];
-        let currQuestion = questionsArray[7];
-        let currArray = hashMap[currQuestion];
-        let functionToRun = currArray[0];
-        let answer = currArray[1];
-        functionToRun(currInput, answer);
+        // let currInput = inputsArray[9];
+        // let currQuestion = questionsArray[9];
+        // let currArray = hashMap[currQuestion];
+        // let functionToRun = currArray[0];
+        // let answer = currArray[1];
+        // functionToRun(currInput, answer);
+    }
+    
+    function enterWorkExperience(input, experiences) {
+        //click button until no more experiences left
+        // let parentSection = //get parent containing all questions as children
+        for (let i = 0; i < experiences.length; i++) {
+
+        }
+
     }
     
     function enterAnswer(input, answer) {
@@ -110,7 +220,6 @@ export default function () {
                 input.dispatchEvent(new KeyboardEvent('keyup', { key: 'Enter', code: 'Enter', bubbles: true }));
 
                 setTimeout(() => {
-                    // const options = Array.from(document.querySelectorAll('[data-uxi-widget-type="multiselectlistitem"]'));
                     const firstOption = document.querySelector('[data-uxi-widget-type="multiselectlistitem"]');
                     if (firstOption) {
                         firstOption.dispatchEvent(new MouseEvent('mousedown', { bubbles: true }));
@@ -124,27 +233,51 @@ export default function () {
         }, 200);
     }
 
-    function chooseFromList(btn, answer) {
-        console.log("worked");
-        //click button
-        
-        //choose from list
-
-        setTimeout(() => {
-            if (btn) {
-                btn.click();
+    function chooseMultipleFromContainer(input, answers) {
+        for (let i = 0; i < answers.length; i++) {
+            setTimeout(() => {
+                input.dispatchEvent(new MouseEvent('mousedown', { bubbles: true }));
+                input.focus();
 
                 setTimeout(() => {
+                    input.value = answers[i];
+                    console.log(answers[i]);
+                    input.dispatchEvent(new InputEvent('input', { bubbles: true }));
 
+                    setTimeout(() => {
+                        input.dispatchEvent(new KeyboardEvent('keydown', { key: 'Enter', code: 'Enter', bubbles: true }));
+                        input.dispatchEvent(new KeyboardEvent('keypress', { key: 'Enter', code: 'Enter', bubbles: true }));
+                        input.dispatchEvent(new KeyboardEvent('keyup', { key: 'Enter', code: 'Enter', bubbles: true }));
+                    }, 100); //waits some time before pressing enter
+                }, 200); //waits some time before inputting answer
+            }, i * 500);  //waits some time before advancing to next thing
+        }
+    }
+
+    //clicks the list button, then finds desired option and clicks it
+    function chooseFromList(listButton, answer) {
+        setTimeout(() => {
+            if (listButton) {
+                listButton.click();
+                setTimeout(() => {
+                    let currId = listButton.getAttribute("aria-controls");
+                    let ul_element = document.querySelector(`ul#${currId}`);
+                    let target_option = Array.from(ul_element.querySelectorAll('li[role="option"]')).find(li => li.querySelector('div')?.textContent.trim() === answer);
+                    target_option.dispatchEvent(new MouseEvent('mousedown', { bubbles: true }));
+                    target_option.click();
                 }, 500); 
             }
         }, 300); 
-
     }
 
 
     function yesOrNo() {
 
+    }
+
+    function upload(input, filePath) {
+        input.click();
+        
     }
 
     function doNothing() {
@@ -155,16 +288,3 @@ export default function () {
     //calls 'fillPage()' until we are on the submit screen
     fillPage();
 }
-
-
-
-/**
-    "how did you hear about us": multiselect container, other
-    "Have you worked for..."
-
-
-
-
-
-
- */
